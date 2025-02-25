27067e3f568c64605c81c578fd2743079c115a04
ByronGary <Byron.gary32@gmail.com>
2025-02-25 14:36:14 -0500

    Add files via upload

---
+++ vbo/src/Plugin/Action/ViewsFileDelete.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\nida_vbo\Plugin\Action;
+
+use Drupal\fancy_file_delete\FancyFileDeleteBatch;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\fancy_file_delete\Entity\UnmanagedFiles;
+use Drupal\file\Entity\File;
+
+/**
+ * Deletes Files.
+ *
+ * @Action(
+ *   id = "views_file_delete",
+ *   label = @Translation("Delete Files nida"),
+ *   type = "",
+ *   pass_view = TRUE
+ * )
+ */
+class ViewsFileDelete extends ViewsBulkOperationsActionBase implements ContainerFactoryPluginInterface
+{
+
+  /**
+   * The Batch Service.
+   *
+   * @var \Drupal\fancy_file_delete\FancyFileDeleteBatch
+   */
+  protected $batch;
+
+  /**
+   * Constructs a new ViewsFileDelete action.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\fancy_file_delete\FancyFileDeleteBatch
+   *   The Batch Service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FancyFileDeleteBatch $batch)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->batch = $batch;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('fancy_file_delete.batch')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function executeMultiple(array $entities)
+  {
+    // Set entities to batch our way.
+    $values = [];
+    foreach ($entities as $entity) {
+      if ($entity instanceof UnmanagedFiles) {
+        $values[] = $entity->getPath();
+      } elseif ($entity instanceof File) {
+        $values[] = $entity->id();
+      }
+    }
+    // Send to batch.
+    $this->batch->setBatch($values, FALSE);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL)
+  {
+    $this->executeMultiple([$entity]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = TRUE)
+  {
+    return $object->access('delete', $account, $return_as_object);
+  }
+}
